69987a88f5989eed958e21ac27fc63fcbe6eeff2
Pedro Henrique <pedrohqesilva@gmail.com>
2020-03-19 16:28:53 -0300

    [domain] [context] ajuste na base entity e inclusão do base map

--- src/Domain/Entities/Entity.cs
+++ src/Domain/Entities/Entity.cs
@@ -3,9 +3,15 @@
     public abstract class Entity
     {
         public long Identificador { get; }
+        public bool Ativo { get; private set; } = true;
 
         protected Entity()
         {
+        }
+
+        protected void Desativar()
+        {
+            Ativo = false;
         }
     }
 }
---
+++ src/Infrastructure.Data.Context/Mappings/EntityMap.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.Context.Mappings
+{
+    public abstract class EntityMap<T> : IEntityTypeConfiguration<T> where T : Entity
+    {
+        public void Configure(EntityTypeBuilder<T> builder)
+        {
+            builder
+                .Property(x => x.Identificador)
+                .IsRequired()
+                .UseHiLo();
+
+            builder
+                .Property(p => p.Ativo)
+                .IsRequired();
+
+            builder
+                .ToTable(GetTableName())
+                .HasKey(x => x.Identificador);
+        }
+
+        protected abstract string GetTableName();
+    }
+}
